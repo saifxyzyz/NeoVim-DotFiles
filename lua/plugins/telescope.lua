afc38ee0abcd5ac11b2a9561968f72cb1452992a
saifxyzyz <saifmujeeb2603@gmail.com>
2025-08-30 14:56:09 +0300

    some more changes to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,11 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
     config = function()
       vim.keymap.set("n", "<space>fd", require("telescope.builtin").find_files)
+      vim.keymap.set("n", "<space>en", function()
+        require("telescope.builtin").find_files({
+          cwd = vim.fn.stdpath("config"),
+        })
+      end)
     end,
   },
 }
