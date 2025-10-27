7ccebfc11c1c12d917337e0345eee5761333e974
saifxyzyz <saifmujeeb2603@gmail.com>
2025-10-27 21:11:27 +0530

    a few jupyter shortcuts

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -1,7 +1,39 @@
 return {
-  {
+  { -- The main table for the molten-nvim plugin
     "benlubas/molten-nvim",
     build = ":UpdateRemotePlugins",
-    lazy = false, -- or true if you prefer lazy loading
+    lazy = false, -- Or true/ft = "python" etc.
+
+    -- Move the keys table INSIDE the plugin's table
+    keys = {
+      {
+        "<leader>jni",
+        "<cmd>MoltenInit<cr>",
+        desc = "[J]upyter [N]eovim [I]nitialize Kernel",
+      },
+      {
+        "<leader>jnx",
+        "<cmd>MoltenEvaluateLine<cr>",
+        desc = "[J]upyter [N]eovim E[x]ecute Line", -- Corrected description
+      },
+      {
+        "<leader>jx",
+        "<cmd>MoltenEvaluateVisual<cr>",
+        desc = "[J]upyter [N]eovim E[x]ecute Cell", -- Corrected description
+      },
+
+
+      -- Add other molten keymaps here if desired
+    },
+
+    -- Optional init/config functions also go inside
+    init = function()
+      -- vim.g.python3_host_prog = '/path/to/your/python3'
+    end,
+    config = function()
+      -- require('molten').setup({})
+    end,
   },
+  -- If you had other plugins defined here, they would follow
+  -- { "another/plugin", ... },
 }
